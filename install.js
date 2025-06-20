841d5346d6721a702547cb767ffd45e8ff5c2e30
DmtKch <dmt.gfn@gmail.com>
2025-06-21 02:09:24 +0300

    Update install.js

--- install.js
+++ install.js
@@ -1,8 +1,1 @@
-const { downloadBrowser } = require('puppeteer/lib/cjs/puppeteer/node/install.js');
 
-downloadBrowser()
-  .then(() => console.log('✅ Chromium downloaded successfully'))
-  .catch(err => {
-    console.error('❌ Failed to download Chromium:', err);
-    process.exit(1);
-  });
